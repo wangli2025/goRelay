9002ae53fff26c433cdcde76abed1781c3cc218a
wangli2025 <wl2026@hotmail.com>
2025-01-01 12:51:18 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gitLog=$(git log -1 --pretty=format:"%H")
-gitLog=9a4bcb7f2eaf9c6b820f42b6b8758d986b38fd1f
+#gitLog=9a4bcb7f2eaf9c6b820f42b6b8758d986b38fd1f
 version=$1
 nowDate=$(date +"%F")
 
